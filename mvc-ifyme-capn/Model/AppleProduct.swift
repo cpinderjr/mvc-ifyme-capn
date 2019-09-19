859c5d9af20cbbdca5ff8b1b792f06d2a39dae71
Carlos Pinder <carlos.pinder@icloud.com>
2019-09-19 11:13:41 -0400

    Created a new class called AppleProduct.

---
+++ mvc-ifyme-capn/Model/AppleProduct.swift
@@ -0,0 +1,21 @@
+//
+//  AppleProduct.swift
+//  mvc-ifyme-capn
+//
+//  Created by Carlos Pinder on 9/19/19.
+//  Copyright © 2019 Carlos Pinder. All rights reserved.
+//
+
+import Foundation
+
+class AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+}
